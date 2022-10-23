6e934aa6b5d10974bcfe15795af982d2f86534be
Mabel Ana Ana <113228105+MabelIY@users.noreply.github.com>
2022-10-23 01:52:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# STRINGS
+TEXTO_1 = "HOLA"
+espacio = " "
+TEXTO_2 = "MUNDO"
+
+#Integers
+numero = 7
+numero_string = str(numero)
+
+#floats
+decimal = 9.6
+
+
+
+#Binarios
+bin = 0b101010
+
+#Hexadecimales
+hex = 0xcd
+
+#Boolenanos
+resultado = 5>2
+
+#Condicional
+
+if True:
+  print("a")
+else:
+  print("b")
+
+
+
+
